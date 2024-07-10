631664f2cff044e21b00c3ff7a3da763af2b99c2
max-js <max@paywhirl.com>
2024-07-10 09:32:21 -0700

    add loadpackagedata

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-# Main file
+
+import csv
+import datetime
+from truck import Truck
+from package import *
+from hashTable import ChainingHashTable
+
+with open("DSA2-Final-Project\CSVFiles\distanceCSV.csv") as distanceCSV:
+    distances = list(csv.reader(distanceCSV))
+
+
+with open("DSA2-Final-Project\CSVFiles\/addressCSV.csv") as addressCSV:
+    addresses = list(csv.reader(addressCSV))
+    
+
+with open("DSA2-Final-Project\CSVFiles\packageCSV.csv") as packageCSV:
+    packages = list(csv.reader(packageCSV))
+
+
+def loadPackageData(packageCSV):
+    with open(packageCSV) as packageData:
+        for row in csv.reader(packageData):
+            package = Package()
+            package.id = int(str.strip(row[0]))
+            package.address = row[1]
+            package.city = row[2]
+            package.state = row[3]
+            package.zip = row[4]
+            package.deliveryTime = row[5]
+            package.weight = row[6]
+            package.rules = row[7]
+"""             printPackageDetails(package)
+            print(" ") """
--- package.py
+++ package.py
@@ -1,14 +1,25 @@
-class Package:
-    def __init__(self, packageId, address, city, state, zipCode, deliveryTime, date, status):
-        self.id = packageId
+class Package(object):
+    def __init__(self, id = None, address = None, city = None, state = None, zipCode = None, deliveryTime = None, weight = None, rules = None, status = 'Hub'):
+        self.id = id
         self.address = address
         self.city = city
         self.state = state
         self.zip = zipCode
         self.deliveryTime = deliveryTime
-        self.date = date
+        self.weight = weight
+        self.rules = rules
         self.status = status
 
     
-    def __str__(self):
-        return (self.id, self.address, self.city, self.state, self.zip, self.deliveryTime, self.date, self.status)
+def printPackageDetails(self):
+    print("Package ID: ", self.id)
+    print("Package Address: ", self.address)
+    print("Package City: ", self.city)
+    print("Package State: ", self.state)
+    print("Package Zip Code: ", self.zip)
+    print("Package Delivery Time: ", self.deliveryTime)
+    print("Package Weight: ", self.weight)
+    print("Package Rules: ", self.rules)
+    print("Package Status: ", self.status)
+    return
+
