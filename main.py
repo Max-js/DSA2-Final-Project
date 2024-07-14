5ebd12f9767bc5341af0af63afc47949ad1dcf57
max-js <max@paywhirl.com>
2024-07-13 18:06:51 -0700

    add get address

--- main.py
+++ main.py
@@ -8,12 +8,9 @@ from hashTable import *
 #Necessary??
 with open("DSA2-Final-Project\CSVFiles\distanceCSV.csv") as distanceCSV:
     distances = list(csv.reader(distanceCSV))
-
-with open("DSA2-Final-Project\CSVFiles\/addressCSV.csv") as addressCSV:
-    addresses = list(csv.reader(addressCSV))
     
-with open("DSA2-Final-Project\CSVFiles\packageCSV.csv") as packageCSV:
-    packages = list(csv.reader(packageCSV))
+""" with open("DSA2-Final-Project\CSVFiles\packageCSV.csv") as packageCSV:
+    packages = list(csv.reader(packageCSV)) """
 
 def loadPackageData(packageCSV, hashTable):
     with open(packageCSV) as packageData:
@@ -36,4 +33,12 @@ table = loadPackageData("DSA2-Final-Project\CSVFiles\packageCSV.csv", table)
 truck1 = Truck("4001 South 700 East", datetime.time(hour=10), 0.0, 16, 18, [1, 13, 14, 15, 16, 20, 29, 30, 31, 34, 37, 40])
 truck2 = Truck("4001 South 700 East", datetime.time(hour=10), 0.0, 16, 18, [3, 6, 12, 17, 18, 19, 21, 22, 23, 24, 26, 27, 35, 36, 38, 39])
 truck3 = Truck("4001 South 700 East", datetime.time(hour=10), 0.0, 16, 18, [2, 4, 5, 6, 7, 8, 9, 10, 11, 25, 28, 32, 33])
+
+
+def getAddress(addressCSV, address):
+    with open(addressCSV) as addressData:
+        for row in csv.reader(addressData):
+            if address in row[2]:
+                return int(row[0])
+
 
