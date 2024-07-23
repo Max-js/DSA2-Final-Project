8b2aada8ebf160635f2b556706150c6001870382
max-js <max@paywhirl.com>
2024-07-23 10:23:49 -0700

    comments for truck and package classes

--- package.py
+++ package.py
@@ -1,5 +1,5 @@
-import datetime
 class Package(object):
+    #Define Package base class and necessary parameters
     def __init__(self, id = None, address = None, city = None, zipCode = None, leftHub = None, deliveryTime = None, deliveryDeadline = None, weight = None, status = 'Hub'):
         self.id = id
         self.address = address
@@ -11,12 +11,15 @@ class Package(object):
         self.weight = weight
         self.status = status
 
+    #Define "pretty" printable format for package class
     def __str__(self):
         return f"\n ID: {self.id}, \n Address: {self.address},\n City: {self.city},\n Zip Code: {self.zip},\n Left Hub: {self.leftHub},\n Delivery Deadline: {self.deliveryDeadline},\n Status: {self.status},\n Delivery Time: {self.deliveryTime},\n Weight: {self.weight} \n"
     
+    #Enables package class printable representation to be consistent when accessing a package object
     def __repr__(self):
         return self.__str__()
     
+    #Returns package status based on a given time frame
     def checkStatus(self, startTime, endTime):
         if self.deliveryTime < startTime or startTime <= self.deliveryTime <= endTime:
             return "Delivered"
--- truck.py
+++ truck.py
@@ -1,4 +1,5 @@
 class Truck(object):
+    #Define Truck base class and necessary parameters
     def __init__(self, id = None, currentLocation = None, currentTime = None, departureTime = None, mileage = 0.0, speed = None, packages = None):
         self.id = id
         self.currentLocation = currentLocation
@@ -8,8 +9,10 @@ class Truck(object):
         self.speed = speed
         self.packages = packages
 
+    #Define "pretty" printable format for truck class
     def __str__(self):
         return f"\n Truck ID: {self.id},\n Current Location: {self.currentLocation},\n Current Time: {self.currentTime},\n Departure Time: {self.departureTime},\n Mileage: {self.mileage},\n Speed: {self.speed},\n Packages: {self.packages} \n"
     
+    #Enables truck class printable representation to be consistent when accessing a truck object
     def __repr__(self):
         return self.__str__()
