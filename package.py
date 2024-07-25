61285d6f247250a4902882eb54d4deb1d14e80c2
Max <108481466+Max-js@users.noreply.github.com>
2024-07-25 10:08:54 -0700

    interface-revisions (#16)
    * update interface to allow for check at specific time
    
    * add truck id to package object

--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 class Package(object):
     #Define Package base class and necessary parameters
-    def __init__(self, id = None, address = None, city = None, zipCode = None, leftHub = None, deliveryTime = None, deliveryDeadline = None, weight = None, status = 'Hub'):
+    def __init__(self, id = None, address = None, city = None, zipCode = None, leftHub = None, truck = None, deliveryTime = None, deliveryDeadline = None, weight = None, status = 'Hub'):
         self.id = id
         self.address = address
         self.city = city
@@ -13,7 +13,7 @@ class Package(object):
 
     #Define "pretty" printable format for package class
     def __str__(self):
-        return f"\n ID: {self.id}, \n Address: {self.address},\n City: {self.city},\n Zip Code: {self.zip},\n Left Hub: {self.leftHub},\n Delivery Deadline: {self.deliveryDeadline},\n Status: {self.status},\n Delivery Time: {self.deliveryTime},\n Weight: {self.weight} \n"
+        return f"\n ID: {self.id}, \n Address: {self.address},\n City: {self.city},\n Zip Code: {self.zip},\n Left Hub: {self.leftHub},\n Truck: {self.truck.id},\n Delivery Deadline: {self.deliveryDeadline},\n Status: {self.status},\n Delivery Time: {self.deliveryTime},\n Weight: {self.weight} \n"
     
     #Enables package class printable representation to be consistent when accessing a package object
     def __repr__(self):
